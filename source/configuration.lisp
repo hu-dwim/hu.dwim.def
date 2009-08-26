650b5ba1315616f838042b352dc15ade7c48b103
levente.meszaros <levente.meszaros@gmail.com>
2009-08-26 09:10:08 +0000

    Tell swank about the readtable.

--- source/configuration.lisp
+++ source/configuration.lisp
@@ -10,5 +10,8 @@
 ;;; they are in a standalone file.
 
 (defun setup-readtable ()
-  (enable-sharp-boolean-syntax)
-  (values))
+  (enable-sharp-boolean-syntax))
+
+#+#.(cl:when (cl:find-package "SWANK") '(:and))
+(register-readtable-for-swank
+ '(:hu.dwim.def) 'setup-readtable)
