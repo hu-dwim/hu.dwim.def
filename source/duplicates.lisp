42d55290d8c8ae41aa0c809c51e2cb26e37d66d6
levente.meszaros <levente.meszaros@gmail.com>
2009-09-22 12:57:02 +0000

    Eliminate usused duplicates.

--- source/duplicates.lisp
+++ source/duplicates.lisp
@@ -8,23 +8,6 @@
 
 ;;; THE CONTENT OF THIS FILE IS COPIED OVER FROM SOME OTHER LIBRARIES TO DECREASE DEPENDENCIES
 
-(defmacro if-bind (var test &body then/else)
-  (assert (first then/else)
-          (then/else)
-          "IF-BIND missing THEN clause.")
-  (destructuring-bind (then &optional else)
-      then/else
-    `(let ((,var ,test))
-       (if ,var ,then ,else))))
-
-(defmacro when-bind (var test &body body)
-  `(if-bind ,var ,test (progn ,@body)))
-
-(defmacro prog1-bind (var ret &body body)
-  `(let ((,var ,ret))
-    ,@body
-    ,var))
-
 ;; from arnesi
 (defmacro defprint-object ((self class-name &key (identity t) (type t) with-package)
                            &body body)
