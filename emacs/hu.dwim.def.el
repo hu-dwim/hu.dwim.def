d61f879735c0305d78e1aaf49247f9ee57f91aa2
attila.lendvai <attila.lendvai@gmail.com>
2009-11-19 21:51:37 +0000

    emacs: also color then using hu.dwim.def:def instead of simply def

--- emacs/hu.dwim.def.el
+++ emacs/hu.dwim.def.el
@@ -8,11 +8,11 @@
 
 (defun hu.dwim.def.lisp-mode-hook ()
   (font-lock-add-keywords
-   nil `(("(\\(def\\)[ 	\n]+(\\([^) 	\n]+\\).*?)[ 	\n(]+\\(.*?\\)[ 	\n)]+"
+   nil `(("(\\(def\\|hu\.dwim\.def:def\\)[ 	\n]+(\\([^) 	\n]+\\).*?)[ 	\n(]+\\(.*?\\)[ 	\n)]+"
           (1 font-lock-keyword-face)
           (2 font-lock-type-face)
           (3 font-lock-function-name-face))
-         ("(\\(def\\)[ 	\n]+\\(.*?\\)[ 	\n(]+\\(.*?\\)[ 	\n)]+"
+         ("(\\(def\\|hu\.dwim\.def:def\\)[ 	\n]+\\(.*?\\)[ 	\n(]+\\(.*?\\)[ 	\n)]+"
           (1 font-lock-keyword-face)
           (2 font-lock-type-face)
           (3 font-lock-function-name-face)))
