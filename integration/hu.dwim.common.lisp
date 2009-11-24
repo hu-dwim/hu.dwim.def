85778028b70b9db587d729e57cde6b2659fc7171
Alexander Gavrilov <angavrilov@gmail.com>
2009-11-24 14:09:02 +0000

    Fix the in-package trick for ECL.
    When loading a fasl ECL seems to resolve referenced
    symbols before executing any code, which breaks the
    in-package replacement trick. Rewrite it explicitly
    using intern and eval to define the macro.

--- integration/hu.dwim.common.lisp
+++ integration/hu.dwim.common.lisp
@@ -9,16 +9,7 @@
 ;;;;;;
 ;;; set up a hu.dwim.common:in-package that shadows cl:in-package and will set up the readtable at compile time.
 
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (unintern 'common-lisp:in-package :hu.dwim.common)
-  (shadow "IN-PACKAGE" :hu.dwim.common))
-
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  ;; this export is not inside the above eval-when for a reason.
-  (export 'hu.dwim.common::in-package :hu.dwim.common)
-  (assert (not (eq 'hu.dwim.common::in-package 'common-lisp:in-package))))
-
-(def macro hu.dwim.common::in-package (package-name)
+(def function expand-in-package (package-name)
   `(progn
      (eval-when (:compile-toplevel :execute)
        (bind ((extended-package (find-extended-package ,(string package-name) :otherwise nil)))
@@ -27,3 +18,14 @@
            ;; external forces make sure that *readtable* is rebound, but not that it's copied. keep that in mind when using :setup-readtable for (def package ...)
            (eval it))))
      (common-lisp:in-package ,package-name)))
+
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (unintern 'common-lisp:in-package :hu.dwim.common)
+  (shadow "IN-PACKAGE" :hu.dwim.common)
+  (let ((in-package-sym (intern "IN-PACKAGE" :hu.dwim.common)))
+    (export in-package-sym :hu.dwim.common)
+    (assert (not (eq in-package-sym 'common-lisp:in-package)))
+    ;; This indirection is necessary for ECL.
+    ;; Please don't try using compile.
+    (eval `(def macro ,in-package-sym (package-name)
+             (expand-in-package package-name)))))
