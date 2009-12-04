77b036382b1f1081d7a375dbb096f0bd1f056a0f
attila.lendvai <attila.lendvai@gmail.com>
2009-12-04 10:13:11 +0000

    tests: make sure the lib itself doesn't run when the suite is loaded; some with-macro tests

--- test/test.lisp
+++ test/test.lisp
@@ -72,61 +72,84 @@
                  (makunbound '+foo+)))
              (macroexpand-1 '(def (special-variable :documentation "documentation") +foo+)))))
 
-(def special-variable *foo*)
+(defvar *with-foo/special*)
 
-(def with-macro with-foo1 (foo)
-  (let ((*foo* foo))
-    (-body-)))
+(defixture test/with-macro/fixture
+  (handler-bind ((style-warning #'muffle-warning))
+    (macrolet ((frob (&body body)
+                 `(progn
+                    ,@(mapcar (lambda (el)
+                                `(finishes (eval ',el)))
+                              body))))
+      (finishes (frob
+                 (def with-macro with-foo1 (foo)
+                   (let ((*with-foo/special* foo))
+                     (-body-)))
+                 (def with-macro with-foo2 (foo bar)
+                   (let* ((local (* 2 foo))
+                          (*with-foo/special* (+ local bar)))
+                     ;; using this syntax, LOCAL is "transferred" into the lexical scope of the body
+                     (-body- local)))
+                 (def with-macro* with-foo3 (foo &key bar)
+                   (let* ((local (* 2 foo))
+                          (*with-foo/special* (+ local bar)))
+                     (-body- local)))
+                 (def with-macro* with-foo4 (lexical-var-name foo &key bar)
+                   (let* ((macro-local-var (* 2 foo))
+                          (*with-foo/special* (+ macro-local-var bar)))
+                     (-body- (macro-local-var lexical-var-name))))
+                 (def with-macro* with-foo5 (new-var-name foo &rest args &key bar (keyword-defaulting (+ 2 2)) &allow-other-keys)
+                   (let* ((local (* 2 foo))
+                          (*with-foo/special* (+ local bar)))
+                     (-body- (local new-var-name))
+                     (list* keyword-defaulting (remove-from-plist args :keyword-defaulting))))
+                 (def with-macro* with-foo6 (arg1 arg2 arg2-new-name)
+                   (let* ((arg1 (* 2 arg1))
+                          (arg2 (+ 2 arg2 )))
+                     (-body- arg1 (arg2 arg2-new-name))))
 
-(def with-macro with-foo2 (foo bar)
-  (let* ((local (* 2 foo))
-         (*foo* (+ local bar)))
-    ;; using this syntax, LOCAL is "transferred" into the lexical scope of the body
-    (-body- local)))
+                 (def with-macro* with-foo7 (&key (keyword-defaulting *with-foo/special*) &allow-other-keys)
+                   ;; *with-foo/special* must not be referenced while macroexpanding
+                   ;; let's also test here when the new name (here 'x') is not coming from a local variable, but it's a quoted symbol
+                   (-body- (keyword-defaulting 'x))))))))
 
-(def with-macro* with-foo3 (foo &key bar)
-  (let* ((local (* 2 foo))
-         (*foo* (+ local bar)))
-    (-body- local)))
+(defsuite* test/with-macro ()
+  (with-fixture test/with-macro/fixture
+    (run-child-tests)))
 
-(def with-macro* with-foo4 (lexical-var-name foo &key bar)
-  (let* ((macro-local-var (* 2 foo))
-         (*foo* (+ macro-local-var bar)))
-    (-body- (macro-local-var lexical-var-name))))
-
-(def with-macro* with-foo5 (new-var-name foo &rest args &key bar (keyword-defaulting (+ 2 2)) &allow-other-keys)
-  (let* ((local (* 2 foo))
-         (*foo* (+ local bar)))
-    (-body- (local new-var-name))
-    (list* keyword-defaulting (remove-from-plist args :keyword-defaulting))))
-
-(def with-macro* with-foo6 (arg1 arg2 arg2-new-name)
-  (let* ((arg1 (* 2 arg1))
-         (arg2 (+ 2 arg2 )))
-    (-body- arg1 (arg2 arg2-new-name))))
-
-(deftest test/with-macro ()
+(deftest (test/with-macro/1 :compile-before-run t) ()
   (with-foo1 42
-    (is (= *foo* 42)))
+    (is (= *with-foo/special* 42))))
+
+(deftest (test/with-macro/2 :compile-before-run t) ()
   (with-foo2 2 6
     (is (= local 4))
-    (is (= *foo* 10)))
+    (is (= *with-foo/special* 10))))
+
+(deftest (test/with-macro/3 :compile-before-run t) ()
   (with-foo3 (2 :bar 6) ; there's a full arglist, because it's a with-macro*
     (is (= local 4))    ; LOCAL is visible in the lexical scope
-    (is (= *foo* 10)))
+    (is (= *with-foo/special* 10))))
+
+(deftest (test/with-macro/4 :compile-before-run t) ()
   (with-foo4 (zork 2 :bar 6) ; there's a full arglist, because it's a with-macro*
     (is (= zork 4))    ; LOCAL is visible in the lexical scope by the name given in VAR-NAME
-    (is (= *foo* 10)))
+    (is (= *with-foo/special* 10))))
+
+(deftest (test/with-macro/5 :compile-before-run t) ()
   (is (equal '(4 :bar 6 :baz 42)
              (with-foo5 (zork 2 :bar 6 :baz 42)
                (is (= zork 4))
-               (is (= *foo* 10)))))
-  (is (equal '(99 :bar 6 :baz 42)
-             (with-foo5 (zork 2 :bar 6 :baz 42 :keyword-defaulting 99)
-               (is (= zork 4))
-               (is (= *foo* 10)))))
+               (is (= *with-foo/special* 10))))))
+
+(deftest (test/with-macro/6 :compile-before-run t) ()
   (bind ((x 2)
          (y 10))
     (with-foo6 (x y bar)
       (is (= arg1 4))
       (is (= bar 12)))))
+
+(deftest (test/with-macro/7 :compile-before-run t) ()
+  (bind ((*with-foo/special* 42))
+    (with-foo7 ()
+      (is (= x 42)))))
