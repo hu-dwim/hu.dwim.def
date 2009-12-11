868c15176e41500480ecf3e0a6e3993a38fcf543
attila.lendvai <attila.lendvai@gmail.com>
2009-12-11 10:42:55 +0000

    update with-macro tests

--- test/with-macro.lisp
+++ test/with-macro.lisp
@@ -12,16 +12,20 @@
 
 (def special-variable *with-foo/special*)
 
+(def test evaluate-with-macro-definition (definition)
+  ;; mostly to have something to trace
+  (finishes (eval definition)))
+
 (def fixture test/with-macro/fixture
   (handler-bind ((style-warning #'muffle-warning))
     (macrolet ((frob (&body body)
                  `(progn
                     ,@(mapcar (lambda (el)
-                                `(finishes (eval ',el)))
+                                `(evaluate-with-macro-definition ',el))
                               body))))
       (finishes (frob
-                 (def with-macro with-foo1 (foo)
-                   (let ((*with-foo/special* foo))
+                 (def with-macro with-foo1 (arg)
+                   (bind ((*with-foo/special* arg))
                      (-body-)))
                  (def with-macro with-foo2 (foo bar)
                    (bind ((local (* 2 foo))
@@ -32,12 +36,12 @@
                    (bind ((local (* 2 foo))
                           (*with-foo/special* (+ local bar)))
                      (-body- local)))
-                 (def with-macro* with-foo4 (lexical-var-name foo &key bar)
-                   (let* ((macro-local-var (* 2 foo))
-                          (*with-foo/special* (+ macro-local-var bar)))
-                     (-body- (macro-local-var lexical-var-name))))
-                 (def with-macro* with-foo5 (new-var-name foo &rest args &key bar (keyword-defaulting (+ 2 2)) &allow-other-keys)
-                   (let* ((local (* 2 foo))
+                 (def (with-macro* :quoted-arguments lexical-var-name) with-foo4 (lexical-var-name foo &key bar)
+                   (bind ((local-var (* 2 foo))
+                          (*with-foo/special* (+ local-var bar)))
+                     (-body- (local-var lexical-var-name))))
+                 (def (with-macro* :quoted-arguments (new-var-name)) with-foo5 (new-var-name foo &rest args &key bar (keyword-defaulting (+ 2 2)) &allow-other-keys)
+                   (bind ((local (* 2 foo))
                           (*with-foo/special* (+ local bar)))
                      (-body- (local new-var-name))
                      (list* keyword-defaulting (remove-from-plist args :keyword-defaulting))))
@@ -45,11 +49,21 @@
                    (bind ((arg1 (* 2 arg1))
                           (arg2 (+ 2 arg2 )))
                      (-body- arg1 (arg2 arg2-new-name))))
-
-                 (def with-macro* with-foo7 (&key (keyword-defaulting *with-foo/special*) &allow-other-keys)
-                   ;; *with-foo/special* must not be referenced while macroexpanding
+                 (def with-macro* with-foo7 (some-variable)
+                   ;; test the situation here when the new name is not coming from a local variable, but it's a quoted symbol
+                   (-body- (some-variable 'constant-new-name)))
+                 (def with-macro* with-foo8 (p1 p2
+                                                &optional (o1 *with-foo/special* o1-provided?) (o2 *with-foo/special*)
+                                                &key (k1 *with-foo/special* k1-provided?) (k2 *with-foo/special*)
+                                                &allow-other-keys)
+                   ;; *with-foo/special* must not be referenced anywhere while macroexpanding
                    ;; let's also test here when the new name (here 'x') is not coming from a local variable, but it's a quoted symbol
-                   (-body- (keyword-defaulting 'x))))))))
+                   (macrolet ((argument-list ()
+                                '(list p1 p2 o1 o1-provided? o2 k1 k1-provided? k2)))
+                     (bind ((argument-list (argument-list)))
+                       (declare (special argument-list))
+                       (-body-))
+                     (argument-list))))))))
 
 (def definer with-macro-test (name &body body)
   `(def test (,name :compile-before-run t) ()
@@ -71,8 +85,8 @@
     (is (= *with-foo/special* 10))))
 
 (def with-macro-test test/with-macro/4
-  (with-foo4 (zork 2 :bar 6) ; there's a full arglist, because it's a with-macro*
-    (is (= zork 4))    ; LOCAL is visible in the lexical scope by the name given in VAR-NAME
+  (with-foo4 (new-name 2 :bar 6)
+    (is (= new-name 4))    ; LOCAL is visible in the lexical scope by the name given in the first required argument
     (is (= *with-foo/special* 10))))
 
 (def with-macro-test test/with-macro/5
@@ -84,11 +98,26 @@
 (def with-macro-test test/with-macro/6
   (bind ((x 2)
          (y 10))
+    ;; the new name of arg2 (in this case 'bar) is implied to be a quoted macro argument due to it is being used as a new name in (-body- ...)
     (with-foo6 (x y bar)
       (is (= arg1 4))
       (is (= bar 12)))))
 
 (def with-macro-test test/with-macro/7
-  (bind ((*with-foo/special* 42))
-    (with-foo7 ()
-      (is (= x 42)))))
+    (with-foo7 (99)
+      (is (= constant-new-name 99))))
+
+(def with-macro-test test/with-macro/8
+  (bind ((*with-foo/special* 'default))
+    (bind ((expected-argument-list '(11 22 default nil default default nil default)))
+      (is (equal (with-foo8 (11 22)
+                   (is (equal (symbol-value 'argument-list) expected-argument-list)))
+                 expected-argument-list)))
+    (bind ((expected-argument-list '(11 22 :o1 t :o2 42 t default)))
+      (is (equal (with-foo8 (11 22 :o1 :o2 :k1 42)
+                   (is (equal (symbol-value 'argument-list) expected-argument-list)))
+                 expected-argument-list)))
+    (bind ((expected-argument-list '(11 22 :o1 t :o2 default nil 43)))
+      (is (equal (with-foo8 (11 22 :o1 :o2 :k2 43)
+                   (is (equal (symbol-value 'argument-list) expected-argument-list)))
+                 expected-argument-list)))))
