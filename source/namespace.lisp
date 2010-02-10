bed95e27d364126611266cb72a2822158444b320
attila.lendvai <attila.lendvai@gmail.com>
2010-02-10 17:34:36 +0000

    added do- and update- accessors to the namespace definer

--- source/namespace.lisp
+++ source/namespace.lisp
@@ -9,11 +9,13 @@
 (def (definer e :available-flags "e") namespace (name &optional definer-args &body definer-forms)
   (bind ((hashtable-var (symbolicate "*" name '#:-namespace*))
          (lock-var (symbolicate "%" name '#:-namespace-lock%))
-         (finder-name (getf -options- :finder-name (symbolicate '#:find- name)))
+         (finder-name    (getf -options- :finder-name (symbolicate '#:find- name)))
          (collector-name (getf -options- :collector-name (symbolicate '#:collect- name '#:-namespace-values)))
-         (iterator-name (getf -options- :iterator-name (symbolicate '#:iterate- name '#:-namespace)))
-         (test-function (getf -options- :test '#'eq))
-         (weakness (getf -options- :weakness)))
+         (iterator-name  (getf -options- :iterator-name (symbolicate '#:iterate- name '#:-namespace)))
+         (do-name        (getf -options- :do-name (symbolicate '#:do- name '#:-namespace)))
+         (updater-name   (getf -options- :updater-name (symbolicate '#:update- name '#:-namespace-value)))
+         (test-function  (getf -options- :test '#'eq))
+         (weakness       (getf -options- :weakness)))
     (remove-from-plistf -options- :test :weakness :finder-name)
     `(progn
        ,@(when (getf -options- :export)
@@ -35,12 +37,27 @@
           `((def function ,collector-name ()
               (bordeaux-threads:with-recursive-lock-held (,lock-var)
                 (hash-table-values ,hashtable-var)))))
-       ;; TODO add a do- iterator macro
        ,@(when iterator-name
            `((def function ,iterator-name (visitor)
                (bordeaux-threads:with-recursive-lock-held (,lock-var)
                  (maphash visitor ,hashtable-var)))))
-       ,@(unless (and (zerop (length definer-args))
-                      (zerop (length definer-forms)))
-          `((def (definer ,@-options-) ,name (name ,@definer-args)
-              `(setf (,',finder-name ',name) ,,@definer-forms)))))))
+       ,@(when do-name
+           `((def macro ,do-name ((key-var value-var &optional return-value) &body body)
+               (with-unique-names (iterator has-more?)
+                 `(bordeaux-threads:with-recursive-lock-held (,',lock-var)
+                    (with-hash-table-iterator (,iterator ,',hashtable-var)
+                      (loop
+                        (bind (((:values ,has-more? ,key-var ,value-var) (,iterator)))
+                          (declare (ignorable ,key-var))
+                          (unless ,has-more?
+                            (return ,return-value))
+                          ,@body))))))))
+       ,@(when updater-name
+           `((def macro ,updater-name ((key value-var &optional exists?-var) &body body)
+               `(bordeaux-threads:with-recursive-lock-held (,',lock-var)
+                  (bind (((:values ,value-var ,exists?-var) (gethash ,key ,',hashtable-var)))
+                    ,@body)))))
+       ,@(unless (zerop (length definer-forms))
+          `((def (definer ,@-options-) ,name ,definer-args
+              ;; locking here _might_ even be useful in some _weird_ situations, but that would prevent having a toplevel (effective) EVAL-ALWAYS inside DEFINER-FORMS
+              ,@definer-forms))))))
