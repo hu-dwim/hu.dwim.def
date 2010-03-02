e9f8267aa687f2da768bd052d343d7ea998545b9
attila.lendvai <attila.lendvai@gmail.com>
2010-03-02 09:51:02 +0000

    tests: with-macro and generics/methods as the functional part

--- test/with-macro.lisp
+++ test/with-macro.lisp
@@ -37,13 +37,13 @@
                           (*with-foo/special* (+ local bar)))
                      ;; name should not change due to nil, and it should be declared ignorable by its original name
                      (-with-macro/body- (local nil :ignorable t))))
-                 (def (with-macro* :quoted-arguments lexical-var-name) with-foo4 (lexical-var-name foo &key bar)
+                 (def (with-macro* :quoted-arguments lexical-var-name) with-foo4 (lexical-var-name foo &key ((:bar bar-local-name)))
                    (bind ((local-var (* 2 foo))
-                          (*with-foo/special* (+ local-var bar)))
+                          (*with-foo/special* (+ local-var bar-local-name)))
                      (-with-macro/body- (local-var lexical-var-name))))
-                 (def (with-macro* :quoted-arguments (new-var-name)) with-foo5 (new-var-name foo &rest args &key bar (keyword-defaulting (+ 2 2)) &allow-other-keys)
+                 (def (with-macro* :quoted-arguments (new-var-name)) with-foo5 (new-var-name foo &rest args &key ((:bar bar-local-name)) (keyword-defaulting (+ 2 2)) &allow-other-keys)
                    (bind ((local (* 2 foo))
-                          (*with-foo/special* (+ local bar)))
+                          (*with-foo/special* (+ local bar-local-name)))
                      (-with-macro/body- (local new-var-name))
                      (list* keyword-defaulting (remove-from-plist args :keyword-defaulting))))
                  (def with-macro* with-foo6 (arg1 arg2 arg2-new-name)
@@ -53,18 +53,9 @@
                  (def with-macro* with-foo7 (some-variable)
                    ;; test the situation here when the new name is not coming from a local variable, but it's a quoted symbol
                    (-with-macro/body- (some-variable 'constant-new-name)))
-                 (def with-macro* with-foo8 (p1 p2
-                                                &optional (o1 *with-foo/special* o1-provided?) (o2 *with-foo/special*)
-                                                &key (k1 *with-foo/special* k1-provided?) (k2 *with-foo/special*)
-                                                &allow-other-keys)
-                   ;; *with-foo/special* must not be referenced anywhere while macroexpanding
-                   ;; let's also test here when the new name (here 'x') is not coming from a local variable, but it's a quoted symbol
-                   (macrolet ((argument-list ()
-                                '(list p1 p2 o1 o1-provided? o2 k1 k1-provided? k2)))
-                     (bind ((argument-list (argument-list)))
-                       (declare (special argument-list))
-                       (-with-macro/body-))
-                     (argument-list)))))))
+                 (define-complex-with-macro with-foo8 function)
+                 (define-complex-with-macro with-foo9 generic)
+                 (define-complex-with-macro with-foo10 method)))))
   (-body-))
 
 (def definer with-macro-test (name &body body)
@@ -109,17 +100,41 @@
     (with-foo7 (99)
       (is (= constant-new-name 99))))
 
+(def macro define-complex-with-macro (name function-definer)
+  `(def (with-macro* :function-definer ,function-definer)
+       ,name (p1 p2
+                 &optional (o1 *with-foo/special* o1-provided?) (o2 *with-foo/special*)
+                 &key ((:k1 local-name-for-k1) *with-foo/special* k1-provided?) (k2 *with-foo/special*)
+                 &allow-other-keys)
+     ;; *with-foo/special* must not be referenced anywhere while macroexpanding
+     ;; let's also test here when the new name (here 'x') is not coming from a local variable, but it's a quoted symbol
+     (macrolet ((argument-list ()
+                  '(list p1 p2 o1 o1-provided? o2 local-name-for-k1 k1-provided? k2)))
+       (bind ((argument-list (argument-list)))
+         (declare (special argument-list))
+         (-with-macro/body-))
+       (argument-list))))
+
+(def macro test-complex-with-macro (name)
+  `(bind ((*with-foo/special* 'default))
+     (bind ((expected-argument-list '(11 22 default nil default default nil default)))
+       (is (equal (,name (11 22)
+                    (is (equal (symbol-value 'argument-list) expected-argument-list)))
+                  expected-argument-list)))
+     (bind ((expected-argument-list '(11 22 :o1 t :o2 42 t default)))
+       (is (equal (,name (11 22 :o1 :o2 :k1 42)
+                    (is (equal (symbol-value 'argument-list) expected-argument-list)))
+                  expected-argument-list)))
+     (bind ((expected-argument-list '(11 22 :o1 t :o2 default nil 43)))
+       (is (equal (,name (11 22 :o1 :o2 :k2 43)
+                    (is (equal (symbol-value 'argument-list) expected-argument-list)))
+                  expected-argument-list)))))
+
 (def with-macro-test test/with-macro/8
-  (bind ((*with-foo/special* 'default))
-    (bind ((expected-argument-list '(11 22 default nil default default nil default)))
-      (is (equal (with-foo8 (11 22)
-                   (is (equal (symbol-value 'argument-list) expected-argument-list)))
-                 expected-argument-list)))
-    (bind ((expected-argument-list '(11 22 :o1 t :o2 42 t default)))
-      (is (equal (with-foo8 (11 22 :o1 :o2 :k1 42)
-                   (is (equal (symbol-value 'argument-list) expected-argument-list)))
-                 expected-argument-list)))
-    (bind ((expected-argument-list '(11 22 :o1 t :o2 default nil 43)))
-      (is (equal (with-foo8 (11 22 :o1 :o2 :k2 43)
-                   (is (equal (symbol-value 'argument-list) expected-argument-list)))
-                 expected-argument-list)))))
+  (test-complex-with-macro with-foo8))
+
+(def with-macro-test test/with-macro/9
+  (test-complex-with-macro with-foo9))
+
+(def with-macro-test test/with-macro/10
+  (test-complex-with-macro with-foo10))
