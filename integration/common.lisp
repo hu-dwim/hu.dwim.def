1206bcb35025d4a2d664751375461dab794f58fd
attila.lendvai <attila.lendvai@gmail.com>
2010-05-21 15:06:16 +0000

    smaller in-package expansion which also binds *package*

--- integration/common.lisp
+++ integration/common.lisp
@@ -9,14 +9,18 @@
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (assert (not (eq 'hu.dwim.common:in-package 'common-lisp:in-package))))
 
+(def function %eval-extended-package-readtable-setup-form (package-name)
+  (bind ((extended-package (find-extended-package package-name :otherwise nil))
+         (*package* (find-package :cl-user)))
+    (awhen (and extended-package
+                (readtable-setup-form-of extended-package))
+      ;; external forces make sure that *readtable* is rebound, but not that it's copied. keep that in mind when using :setup-readtable for (def package ...)
+      (eval it))))
+
 (locally
     (declare #+sbcl(sb-ext:muffle-conditions sb-kernel:redefinition-warning))
   (def macro hu.dwim.common:in-package (package-name)
     `(progn
        (eval-when (:compile-toplevel :execute)
-         (bind ((extended-package (find-extended-package ,(string package-name) :otherwise nil)))
-           (awhen (and extended-package
-                       (readtable-setup-form-of extended-package))
-             ;; external forces make sure that *readtable* is rebound, but not that it's copied. keep that in mind when using :setup-readtable for (def package ...)
-             (eval it))))
+         (%eval-extended-package-readtable-setup-form ,(string package-name)))
        (common-lisp:in-package ,package-name))))
